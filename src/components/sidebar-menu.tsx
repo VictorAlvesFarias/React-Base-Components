9b0dd7513b63ec1f82769c0a395505e49a8d95c3
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2026-05-23 17:47:08 -0300

    Update

--- src/components/sidebar-menu.tsx
+++ src/components/sidebar-menu.tsx
@@ -10,17 +10,17 @@ function MenuContainer(props: IMenuContainerProps) {
 
     return (
         <>
-            <div className={'md:flex hidden overflow-y-auto ' + props.className}>
+            <div aria-checked={open} className={'md:flex hidden overflow-y-auto ' + props.className}>
                 {props.children}
             </div>
-            <div className={`md:hidden fixed   z-50 w-full flex top-0 h-screen transition-all duration-500 ${open ? "right-0" : "right-full"}`}>
+            <div aria-checked={open} className={`md:hidden fixed   z-50 w-full flex top-0 h-screen transition-all duration-500 aria-checked:right-0 right-full`}>
                 <div className={'flex overflow-y-auto md:hidden ' + props.className} role="dialog" aria-modal="true">
                     {props.children}
                 </div>
                 <div className={'flex-1 pr-12 w-full h-full '} onClick={() => setOpen(false)}>
                 </div>
             </div>
-            <div className={'flex-1 pr-12 z-40 fixed flex md:hidden  top-0 h-full w-full transition-all ' + (open ? "right-0 delay-0" : "right-full delay-500 ")}>
+            <div aria-checked={open} className={'flex-1 pr-12 z-40 fixed flex md:hidden  top-0 h-full w-full transition-all aria-checked:right-0 right-full'}>
                 <div aria-checked={open} className={'flex-1 pr-12  fixed blur-sm h-full w-full bg-black transition-all duration-500 aria-checked:bg-opacity-30 bg-opacity-0'}>
                 </div>
             </div>
